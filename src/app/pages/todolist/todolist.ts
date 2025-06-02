3650368057d6b98df56ad983886585006649224f
João Pedro Morbeck <jpmorbeck45@gmail.com>
2025-06-02 14:55:23 -0300

    Criação do projeto e configuração das rotas

---
+++ src/app/pages/todolist/todolist.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-todolist',
+  imports: [RouterLink],
+  templateUrl: './todolist.html',
+  styleUrl: './todolist.css'
+})
+export class Todolist {
+
+}
---
+++ src/app/pages/user/user.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  imports: [RouterLink],
+  templateUrl: './user.html',
+  styleUrl: './user.css'
+})
+export class User {
+
+}
